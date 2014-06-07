2dcd1b3674c84e13187ee819c14171abc6cea3ee
Zygmunt Łenyk <zygmuntlenyk@gmail.com>
2014-06-07 16:13:42 +0200

    add files' ;

---
+++ dots/scripts/game.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+	prepareNewGame();
+}
+
+function prepareNewGame(){
+	
+	timer = new Timer();
+	board = new Board();
+	board.prepareNewBoard();
+	timer.initTimer();
+	
+	var results = new Results();
+}
+function startNewGame(){
+	timer.stopTimer();
+	board.destroyBoard();
+	prepareNewGame();
+}
+function click(){
+	var count = board.getClickedCount();
+	if(count == 1){
+		timer.startTimer();
+	}
+	if(count == size*size){
+		timer.stopTimer();
+	}
+}
+function getResults(filePath){
+	var results = new Results();
+	results.showResults(filePath);
+}
