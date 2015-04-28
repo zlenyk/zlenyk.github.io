c9d6af36f17c88c1ad61283b94e8ee7a9418b692
Zygmunt <zygmuntlenyk@gmail.com>
2015-04-28 23:40:35 +0200

    gallery conception

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('.menu > li').mouseover(function(){
-        $(this).find('ul').css('visibility','visible');
     });
     $('.menu > li').mouseout(function(){
-        $(this).find('ul').css('visibility','hidden');
     });
 });
