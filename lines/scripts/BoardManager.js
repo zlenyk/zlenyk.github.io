f22d61a53ba9df44120f1ecf9d7a969b1ec5901f
zlenyk <zygmuntlenyk@gmail.com>
2016-02-01 22:58:58 +0100

    testing end of game, tests, automatic play

--- lines/scripts/BoardManager.js
+++ lines/scripts/BoardManager.js
@@ -13,14 +13,14 @@ var BoardManager = function(canvas){
     this.init = function(){
         this.getCoords(20);
         this.prepareCirclesArray();
-    }
+    };
     this.drawBoard = function(){
         var context = this.canvas.getContext('2d'); 
         context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.drawLines(this.initialLines,1,'black');
         this.drawLines(this.additionalLines,3,'#005100');
         this.drawAllCircles();
-    }
+    };
     this.getCoords = function(gap){
         var h = this.canvas.height;
         var w = this.canvas.width;
@@ -32,7 +32,85 @@ var BoardManager = function(canvas){
             this.XCoords.push(i);
             this.initialLines.push(new Line(new Point(i,0),new Point(i,h)));
         }
-    }
+    };
+    /* return unchecked point between given points inclusively
+     */
+    this.getUncheckedPoint = function(p1,p2){
+        var nArray = this.getPointsBetween(p1,p2);
+        for(var i=0;i<nArray.length;i++){
+            if(this.circles[nArray[i].x][nArray[i].y] != 1)
+                return nArray[i];
+        }
+        return -1;
+    };
+    this.getPointsFormingLine = function(){
+        for(var i=0;i<this.XCoords.length;i++){
+            for(var j=0;j<this.YCoords.length;j++){
+                var p = new Point(i,j);
+                var goodNeighbour = this.checkNeighbourPoints(p,5);
+                if(goodNeighbour.x != -1)
+                    return [this.indexToCoords(p),this.indexToCoords(goodNeighbour)];
+            }
+        }
+        return [-1,-1];
+    };
+    this.getTargetPoints = function(p){
+        var tab = [[1,0],[0,1],[1,1],[1,-1]];
+        var retArray = []; 
+        for(var i=0;i<tab.length;i++){
+            var newX = p.x+4*tab[i][0];
+            var newY = p.y+4*tab[i][1];
+            if(newX>=0 && newX<this.XCoords.length && newY>=0 && newY<this.YCoords.length)
+                retArray.push(new Point(newX,newY));
+        }
+        return retArray;
+    };
+    /* True if in neighbourhood(5) of point lies given number of points
+     * in on line and are not connected
+     * return point to which it is done
+     */
+    this.checkNeighbourPoints = function(p1,number){
+        var neighbours = this.getTargetPoints(p1);
+        for(var i=0;i<neighbours.length;i++){
+            var p2 = neighbours[i];
+            var neighArray = this.getPointsBetween(p1,p2);
+            var count = 0;
+            for(var j=0;j<neighArray.length;j++){
+                if(this.circles[neighArray[j].x][neighArray[j].y] == 1){
+                    count++;
+                }
+            }
+            if(count !== number) continue;
+            var ok = true;
+            for(j=0;j<neighArray.length-1;j++){
+                if(this.arrayHasLine(
+                            this.additionalLines,
+                            new Line(this.indexToCoords(neighArray[j]),
+                                    this.indexToCoords(neighArray[j+1])))){
+                    ok = false;
+                    break;
+                }
+            }
+            if(!ok) continue;
+            return p2;
+        }
+        return new Point(-1,-1);
+    };
+    this.getClickablePoint = function(){
+        for(var i=0;i<this.XCoords.length;i++){
+            for(var j=0;j<this.YCoords.length;j++){
+                var p = new Point(i,j);
+                var goodNeighbour = this.checkNeighbourPoints(p,4);
+                if(goodNeighbour.x!=-1){
+                    return this.indexToCoords(this.getUncheckedPoint(p,goodNeighbour));
+                }
+            }
+        }
+        return new Point(-1,-1);
+    };
+    this.checkAllPointsNeighbours = function(){
+        return this.getClickablePoint().x != -1;
+    };
     this.prepareCirclesArray = function(){
         this.circles = new Array(this.YCoords.length);
         for(var i=0;i<this.XCoords.length;i++){
@@ -42,53 +120,72 @@ var BoardManager = function(canvas){
             }
         }
         for(i=0;i<this.initialPoints.length;i++){
-            this.circles[this.XCoords.length/2+this.initialPoints[i][0]]
-            [this.YCoords.length/2+this.initialPoints[i][1]] = 1;
+            this.circles[this.XCoords.length/2+this.initialPoints[i][0]][this.YCoords.length/2+this.initialPoints[i][1]] = 1;
         }
-    }
+    };
     this.indexToCoords = function(p){
         return new Point(this.XCoords[p.x],this.YCoords[p.y]);
-    }
+    };
     this.addLine = function(p1,p2){
         if(this.pointsFormGoodLine(p1,p2)){
             var coordsP1 = this.indexToCoords(p1);
             var coordsP2 = this.indexToCoords(p1);
             var line = new Line(this.indexToCoords(p1),this.indexToCoords(p2));
-            if(this.arrayHasLine(this.additionalLines,line) == false){
+            if(this.arrayHasLine(this.additionalLines,line) === false){
                 this.additionalLines.push(line);
                 this.drawBoard();
                 return true;
             }
         }
         return false;
-    }
+    };
+    /* returns array of points that lies between two given(inclusive)
+     */
+    this.getPointsBetween = function(p1,p2){
+        var retArray = [];
+        var xd = p2.x-p1.x;
+        var yd = p2.y-p1.y;
+        if(Math.abs(xd)!==Math.abs(yd) && Math.abs(xd)!==0 && Math.abs(yd)!==0){
+            return retArray;
+        }
+        for(var i=0;i<=Math.max(Math.abs(xd),Math.abs(yd));i++){
+            var p;
+            var newX = p1.x;
+            var newY = p1.y;
+            if(xd !== 0){
+                newX = p1.x+(i*xd)/Math.abs(xd); 
+            }
+            if(yd !== 0){
+                newY = p1.y+(i*yd)/Math.abs(yd); 
+            }
+            if(newX>=0 && newX<this.XCoords.length && newY>=0 && newY<this.YCoords.length){
+                retArray.push(new Point(newX,newY));
+            }
+        }
+        return retArray;
+    };
     this.pointsFormGoodLine = function(p1,p2){
-        var x1 = p1.x;var y1 = p1.y;
-        var x2 = p2.x;var y2 = p2.y;
-        var xd = x1-x2;
-        var yd = y1-y2;
-        if(!((Math.abs(xd) == 4 || Math.abs(xd) == 0) &&
-                    (Math.abs(yd) == 4 || Math.abs(yd) == 0) &&
-                    (xd != 0 || yd != 0))) 
-            return false;
-        for(var i=1;i<=3;i++){
-            if(this.circles[x2+(xd/4)*i][y2+(yd/4)*i] != 1){
+        var retArray = this.getPointsBetween(p1,p2);
+        if(retArray.length != 5) return false;
+        for(var i=0;i<retArray.length;i++){
+            if(this.circles[retArray[i].x][retArray[i].y] < 1){
                 return false;
             }
         }
         return true;
-    }
+    };
     /*  true if are on same line
      * */
     this.arrayHasLine = function(array,line){
         try{
         for(var i=0;i<array.length;i++){
-            if(line.areSame(array[i]) || line.lineOnOther(array[i]))
+            if(line.areSame(array[i]) || line.lineOnOther(array[i])){
                 return true;
+            }
         }
         }catch(err){alert(err);}
         return false;
-    }
+    };
     /*
      * Just drawing circles from array circles
      */
@@ -102,7 +199,7 @@ var BoardManager = function(canvas){
                 }
             }
         }
-    }
+    };
     /*
      *  Draw lines from argument array
      */
@@ -110,68 +207,86 @@ var BoardManager = function(canvas){
         linesArray.forEach(function(l){
             drawLine(canvas,l,width,color);
         });
-    }
-    this.drawNewPoint = function(x,y){
-        var r = false;
-        if(this.circles[x][y] === 0){
-            this.circles[x][y] = 1;
-            r = true;
-            this.drawBoard();
-        }
-        return r;
-    }
-    this.erasePoint = function(x,y){
+    };
+    this.erasePoint = function(p){
+        var r = this.pointClicked(p);
+        var res = r[0];var indexP = r[1];
+        var x = indexP.x;
+        var y = indexP.y;
         if(this.circles[x][y] > 0){
             this.circles[x][y] = 0;
             this.drawBoard();
         }
-    }
-    this.checkPoint = function(x,y){
-        var r = false;
-        if(this.circles[x][y] > 0){
-            if(this.circles[x][y] === 1)
-                this.circles[x][y] = 2;
-            else if(this.circles[x][y] === 2)
-                this.circles[x][y] = 1;
-            r = true;
+    };
+    this.drawNewPoint = function(p){
+        var r = this.pointClicked(p);
+        var res = r[0];var indexP = r[1];
+        if(res === false) return false;
+        var x = indexP.x;
+        var y = indexP.y;
+        if(this.circles[x][y] === 0){
+            this.circles[x][y] = 1;
             this.drawBoard();
+            return true;
         }
-        return r;
-    }
+        return false;
+    };
+    this.checkPoint = function(p){
+        var r = this.pointClicked(p);
+        var res = r[0];var indexP = r[1];
+        if(res === false) return false;
+        var x = indexP.x;
+        var y = indexP.y;
+        if(this.circles[x][y] === 1){
+            this.circles[x][y] = 2;
+        }
+        else if(this.circles[x][y] === 2){
+            this.circles[x][y] = 1;
+        }
+        this.drawBoard();
+        return true;
+    };
     this.checkedPoints = function(){
         var res = [];
-        for(var i=0;i<this.XCoords.length;i++)
-            for(var j=0;j<this.YCoords.length;j++)
-                if(this.circles[i][j] == 2)
+        for(var i=0;i<this.XCoords.length;i++){
+            for(var j=0;j<this.YCoords.length;j++){
+                if(this.circles[i][j] == 2){
                     res.push(new Point(i,j));
+                }
+            }
+        }
         return res;
-    }
+    };
     this.flushCheck = function(){
-        for(var i=0;i<this.XCoords.length;i++)
-            for(var j=0;j<this.YCoords.length;j++)
+        for(var i=0;i<this.XCoords.length;i++){
+            for(var j=0;j<this.YCoords.length;j++){
                 if(this.circles[i][j] == 2){
                     this.circles[i][j] = 1;
                 }
+            }
+        }
         this.drawBoard();
-    }
-    /* returns intersection that near coordinates(argument)
+    };
+    /* returns index point and res-if something is clicked 
      */
-    this.checkLineClicked = function(x,y){
+    this.pointClicked = function(p){
         var mistake = 5;
         var Xclicked = -1;
         var Yclicked = -1;
+        var res = true;
         for(var i=0;i<this.XCoords.length;i++){
-            if(Math.abs(this.XCoords[i]-x) <= mistake){
+            if(Math.abs(this.XCoords[i]-p.x) <= mistake){
                 Xclicked = i;
                 break;
             }
         }
         for(i=0;i<this.YCoords.length;i++){
-            if(Math.abs(this.YCoords[i]-y) <= mistake){
+            if(Math.abs(this.YCoords[i]-p.y) <= mistake){
                 Yclicked = i;
                 break;
             }
         }
-        return [Xclicked,Yclicked];
-    }
-}
+        if(Xclicked == -1 || Yclicked == -1) res = false;
+        return [res,new Point(Xclicked,Yclicked)];
+    };
+};
--- lines/scripts/game.js
+++ lines/scripts/game.js
@@ -1,38 +1,72 @@
 'use strict';
 window.onload = function(){
-        prepareNewGame();
+    prepareNewGame();
 };
 
 var canvas;
 var dotMove; //true,false (is now a dot or line move)
 var score;
-var lastDot;
+var lastPoint;
 var boardManager;
 
 function prepareNewGame(){
     canvas = document.getElementById('gameCanvas');
-    try{ 
     boardManager = new BoardManager(canvas);
     boardManager.init();
     boardManager.drawBoard();
-    }catch(err){alert(err);}
-    lastDot = [-1,-1];
+    lastPoint - new Point(-1,-1);
     canvas.addEventListener('mousedown',boardClicked,false);
     dotMove = true;
     score = 0;
     refreshScore();
 }
 function undoLastDot(){
-    if(lastDot[0] != -1 && !dotMove){
-        boardManager.erasePoint(lastDot[0],lastDot[1]);
-        lastDot = [-1,-1];
+    if(lastPoint.x != -1 && !dotMove){
+        boardManager.erasePoint(lastPoint);
+        lastDot = new Point(-1,-1);
         dotMove = !dotMove;
         boardManager.flushCheck();
     }
 }
+function endOfGame(){
+    alert("Your score: "+score+"\n"+"Congratulations!");
+    prepareNewGame();
+}
+function checkEndOfGame(){
+    var p = boardManager.getClickablePoint();
+    if(p.x == -1){
+        return true;
+    }
+}
 function refreshScore(){
-   var div = document.getElementById('score');
-   div.innerHTML = "Score: " + score;
+    var div = document.getElementById('score');
+    div.innerHTML = "Score: " + score;
+}
+function test(){
+    var div = document.getElementById('test');
+    div.innerHTML = '';
+    var arr = boardManager.getPointsBetween(new Point(10,10),new Point(10,6)); 
+    for(var i=0;i<arr.length;i++){
+        div.innerHTML += arr[i].x + ' ' + arr[i].y + '\n';
+    }
+}
+function playAutomatically(){
+    var myInterval = setInterval(function(){
+        if(dotMove){
+            var p = boardManager.getClickablePoint();
+            checkBoard(p);
+        }
+        else{
+            var pA = boardManager.getPointsFormingLine();
+            checkBoard((pA[0]));
+            checkBoard(pA[1]);
+            searchForNewLine();
+        }
+        if(checkEndOfGame()){
+            clearInterval(myInterval);
+            alert('end');
+        }
+    }, 100);
 }
 /* Check what intersetcion was clicked,
  * allows dots to be drawn/colored when dotMove is true/false 
@@ -41,20 +75,26 @@ function refreshScore(){
 function boardClicked(event){
     var x = event.pageX - canvas.offsetLeft;
     var y = event.pageY - canvas.offsetTop;
-    var indexClicked = boardManager.checkLineClicked(x,y);
-    if(indexClicked[0] > -1 && indexClicked[1] > -1){
-        if(dotMove){
-            var res=boardManager.drawNewPoint(indexClicked[0],indexClicked[1]);
-            if(res == true){
-               dotMove = !dotMove;
-               lastDot = [indexClicked[0],indexClicked[1]];
-            }
-        }else if(!dotMove){
-            boardManager.checkPoint(indexClicked[0],indexClicked[1]);
-            searchForNewLine();
-        }
+    checkBoard(new Point(x,y));
+    if(!dotMove){
+        searchForNewLine();
+        if(checkEndOfGame())
+            endGame();
     }
 }
+function checkBoard(p){
+    if(dotMove){
+        var res=boardManager.drawNewPoint(p);
+        if(res == true){
+            dotMove = !dotMove;
+            lastPoint = p;
+        }
+    }else if(!dotMove){
+        boardManager.checkPoint(p);
+    }
+}
+/* Checks if 2 points are added and tries to add line
+*/
 function searchForNewLine(){
     var indexPointsArray = boardManager.checkedPoints();
     if(indexPointsArray.length === 2){
@@ -65,5 +105,4 @@ function searchForNewLine(){
         }
         boardManager.flushCheck();
     }
-
 }
